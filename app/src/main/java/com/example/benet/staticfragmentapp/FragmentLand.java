8f842d36f2e3b0dd8a31112813ef72dab36f00fc
BenetPerarnau <benetperarnau@hotmail.com>
2015-02-10 23:31:52 +0100

    1r

---
+++ app/src/main/java/com/example/benet/staticfragmentapp/FragmentLand.java
@@ -0,0 +1,19 @@
+package com.example.benet.staticfragmentapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Benet on 10/02/15.
+ */
+public class FragmentLand extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_land,container,false);
+    }
+}
